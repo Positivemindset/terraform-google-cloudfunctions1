790643381d6debef814150abc6aada0e15b3361f
Harsha Evuri <harsha.evuri@gmail.com>
2023-05-30 09:15:24 +0100

    added outputs.tf

---
+++ test/fixtures/default/outputs.tf
@@ -0,0 +1,389 @@
+output "owner" {
+  value       = var.owner
+  description = "Owner of the resource"
+}
+
+output "cmdb_appid" {
+  value       = var.cmdb_appid
+  description = "CMDB App ID"
+}
+
+output "cost_centre" {
+  value       = var.cost_centre
+  description = "Cost centre"
+}
+
+output "data_classification" {
+  value       = var.data_classification
+  description = "Data classification"
+}
+
+output "environment" {
+  value       = var.environment
+  description = "Environment"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "Project ID"
+}
+
+output "available_memory_mb" {
+  value       = var.available_memory_mb
+  description = "Available memory in MB"
+}
+
+output "timeout" {
+  value       = var.timeout
+  description = "Timeout in seconds"
+}
+
+output "environment_vars" {
+  value       = var.environment_vars
+  description = "Environment variables"
+}
+
+output "ingress_settings" {
+  value       = var.ingress_settings
+  description = "Ingress settings"
+}
+
+output "vpc_connector_egress_settings" {
+  value       = var.vpc_connector_egress_settings
+  description = "VPC connector egress settings"
+}
+
+output "vpc_connector" {
+  value       = var.vpc_connector
+  description = "VPC connector"
+}
+
+output "service_account_email" {
+  value       = var.service_account_email
+  description = "Service account email"
+}
+
+output "max_instances" {
+  value       = var.max_instances
+  description = "Maximum number of instances"
+}
+
+output "github_url" {
+  value       = var.github_url
+  description = "GitHub URL"
+}
+
+output "kms_key_name" {
+  value       = var.kms_key_name
+  description = "KMS key name"
+}
+
+output "entry_point_python" {
+  value       = var.entry_point_python
+  description = "Entry point for Python"
+}
+
+output "entry_point_dotnet" {
+  value       = var.entry_point_dotnet
+  description = "Entry point for Dotnet"
+}
+
+output "http_description_python" {
+  value       = var.http_description_python
+  description = "HTTP description for Python"
+}
+
+output "http_function_name_python" {
+  value       = var.http_function_name_python
+  description = "HTTP function name for Python"
+}
+
+output "http_runtime_python" {
+  value       = var.http_runtime_python
+  description = "HTTP runtime for Python"
+}
+
+output "http_local_folder_python" {
+  value       = var.http_local_folder_python
+  description = "HTTP local folder for Python"
+}
+
+output "http_folder_name_python" {
+  value       = var.http_folder_name_python
+  description = "HTTP folder name for Python"
+}
+
+output "http_trigger_type_python" {
+  value       = var.http_trigger_type_python
+  description = "HTTP trigger type for Python"
+}
+
+output "http_encryption_type_python" {
+  value       = var.http_encryption_type_python
+  description = "HTTP encryption type for Python"
+}
+
+output "http_zip_name_python" {
+  value       = var.http_zip_name_python
+  description = "HTTP zip name for Python"
+}
+
+output "http_function_archive_bucket_name_python" {
+  value       = var.http_function_archive_bucket_name_python
+  description = "HTTP function archive bucket name for Python"
+}
+
+output "pubsub_description_python" {
+  value       = var.pubsub_description_python
+  description = "Pub/Sub description for Python"
+}
+
+output "pubsub_function_name_python" {
+  value       = var.pubsub_function_name_python
+  description = "Pub/Sub function name for Python"
+}
+
+output "pubsub_runtime_python" {
+  value       = var.pubsub_runtime_python
+  description = "Pub/Sub runtime for Python"
+}
+
+output "pubsub_folder_name_python" {
+  value       = var.pubsub_folder_name_python
+  description = "Pub/Sub folder name for Python"
+}
+
+output "pubsub_trigger_type_python" {
+  value       = var.pubsub_trigger_type_python
+  description = "Pub/Sub trigger type for Python"
+}
+
+output "pubsub_trigger_event_type_pubsub_python" {
+  value       = var.pubsub_trigger_event_type_pubsub_python
+  description = "Pub/Sub trigger event type for Python"
+}
+
+output "pubsub_trigger_event_resource_python" {
+  value       = var.pubsub_trigger_event_resource_python
+  description = "Pub/Sub trigger event resource for Python"
+}
+
+output "pubsub_encryption_type_python" {
+  value       = var.pubsub_encryption_type_python
+  description = "Pub/Sub encryption type for Python"
+}
+
+output "pubsub_zip_name_python" {
+  value       = var.pubsub_zip_name_python
+  description = "Pub/Sub zip name for Python"
+}
+
+output "pubsub_function_archive_bucket_name_python" {
+  value       = var.pubsub_function_archive_bucket_name_python
+  description = "Pub/Sub function archive bucket name for Python"
+}
+
+output "pubsub_local_folder_python" {
+  value       = var.pubsub_local_folder_python
+  description = "Pub/Sub local folder for Python"
+}
+
+output "storage_description_python" {
+  value       = var.storage_description_python
+  description = "Storage description for Python"
+}
+
+output "storage_function_name_python" {
+  value       = var.storage_function_name_python
+  description = "Storage function name for Python"
+}
+
+output "storage_runtime_python" {
+  value       = var.storage_runtime_python
+  description = "Storage runtime for Python"
+}
+
+output "storage_folder_name_python" {
+  value       = var.storage_folder_name_python
+  description = "Storage folder name for Python"
+}
+
+output "storage_trigger_type_python" {
+  value       = var.storage_trigger_type_python
+  description = "Storage trigger type for Python"
+}
+
+output "storage_trigger_event_resource_python" {
+  value       = var.storage_trigger_event_resource_python
+  description = "Storage trigger event resource for Python"
+}
+
+output "storage_encryption_type_python" {
+  value       = var.storage_encryption_type_python
+  description = "Storage encryption type for Python"
+}
+
+output "storage_zip_name_python" {
+  value       = var.storage_zip_name_python
+  description = "Storage zip name for Python"
+}
+
+output "storage_function_archive_bucket_name_python" {
+  value       = var.storage_function_archive_bucket_name_python
+  description = "Storage function archive bucket name for Python"
+}
+
+output "storage_local_folder_python" {
+  value       = var.storage_local_folder_python
+  description = "Storage local folder for Python"
+}
+
+output "http_description_dotnet" {
+  value       = var.http_description_dotnet
+  description = "HTTP description for Dotnet"
+}
+
+output "http_function_name_dotnet" {
+  value       = var.http_function_name_dotnet
+  description = "HTTP function name for Dotnet"
+}
+
+output "http_runtime_dotnet" {
+  value       = var.http_runtime_dotnet
+  description = "HTTP runtime for Dotnet"
+}
+
+output "http_folder_name_dotnet" {
+  value       = var.http_folder_name_dotnet
+  description = "HTTP folder name for Dotnet"
+}
+
+output "http_trigger_type_dotnet" {
+  value       = var.http_trigger_type_dotnet
+  description = "HTTP trigger type for Dotnet"
+}
+
+output "http_encryption_type_dotnet" {
+  value       = var.http_encryption_type_dotnet
+  description = "HTTP encryption type for Dotnet"
+}
+
+output "http_zip_name_dotnet" {
+  value       = var.http_zip_name_dotnet
+  description = "HTTP zip name for Dotnet"
+}
+
+output "http_function_archive_bucket_name_dotnet" {
+  value       = var.http_function_archive_bucket_name_dotnet
+  description = "HTTP function archive bucket name for Dotnet"
+}
+
+output "http_local_folder_dotnet" {
+  value       = var.http_local_folder_dotnet
+  description = "HTTP local folder for Dotnet"
+}
+
+output "pubsub_description_dotnet" {
+  value       = var.pubsub_description_dotnet
+  description = "Pub/Sub description for Dotnet"
+}
+
+output "pubsub_function_name_dotnet" {
+  value       = var.pubsub_function_name_dotnet
+  description = "Pub/Sub function name for Dotnet"
+}
+
+output "pubsub_runtime_dotnet" {
+  value       = var.pubsub_runtime_dotnet
+  description = "Pub/Sub runtime for Dotnet"
+}
+
+output "pubsub_folder_name_dotnet" {
+  value       = var.pubsub_folder_name_dotnet
+  description = "Pub/Sub folder name for Dotnet"
+}
+
+output "pubsub_trigger_type_dotnet" {
+  value       = var.pubsub_trigger_type_dotnet
+  description = "Pub/Sub trigger type for Dotnet"
+}
+
+output "pubsub_trigger_event_type_pubsub_dotnet" {
+  value       = var.pubsub_trigger_event_type_pubsub_dotnet
+  description = "Pub/Sub trigger event type for Dotnet"
+}
+
+output "pubsub_trigger_event_resource_dotnet" {
+  value       = var.pubsub_trigger_event_resource_dotnet
+  description = "Pub/Sub trigger event resource for Dotnet"
+}
+
+output "pubsub_encryption_type_dotnet" {
+  value       = var.pubsub_encryption_type_dotnet
+  description = "Pub/Sub encryption type for Dotnet"
+}
+
+output "pubsub_zip_name_dotnet" {
+  value       = var.pubsub_zip_name_dotnet
+  description = "Pub/Sub zip name for Dotnet"
+}
+
+output "pubsub_function_archive_bucket_name_dotnet" {
+  value       = var.pubsub_function_archive_bucket_name_dotnet
+  description = "Pub/Sub function archive bucket name for Dotnet"
+}
+
+output "pubsub_local_folder_dotnet" {
+  value       = var.pubsub_local_folder_dotnet
+  description = "Pub/Sub local folder for Dotnet"
+}
+
+output "storage_description_dotnet" {
+  value       = var.storage_description_dotnet
+  description = "Storage description for Dotnet"
+}
+
+output "storage_function_name_dotnet" {
+  value       = var.storage_function_name_dotnet
+  description = "Storage function name for Dotnet"
+}
+
+output "storage_runtime_dotnet" {
+  value       = var.storage_runtime_dotnet
+  description = "Storage runtime for Dotnet"
+}
+
+output "storage_folder_name_dotnet" {
+  value       = var.storage_folder_name_dotnet
+  description = "Storage folder name for Dotnet"
+}
+
+output "storage_trigger_type_dotnet" {
+  value       = var.storage_trigger_type_dotnet
+  description = "Storage trigger type for Dotnet"
+}
+
+output "storage_trigger_event_resource_dotnet" {
+  value       = var.storage_trigger_event_resource_dotnet
+  description = "Storage trigger event resource for Dotnet"
+}
+
+output "storage_encryption_type_dotnet" {
+  value       = var.storage_encryption_type_dotnet
+  description = "Storage encryption type for Dotnet"
+}
+
+output "storage_zip_name_dotnet" {
+  value       = var.storage_zip_name_dotnet
+  description = "Storage zip name for Dotnet"
+}
+
+output "storage_function_archive_bucket_name_dotnet" {
+  value       = var.storage_function_archive_bucket_name_dotnet
+  description = "Storage function archive bucket name for Dotnet"
+}
+
+output "storage_local_folder_dotnet" {
+  value       = var.storage_local_folder_dotnet
+  description = "Storage local folder for Dotnet"
+}
--- test/integration/default/controls/cf-inspec-tests.rb
+++ test/integration/default/controls/cf-inspec-tests.rb
@@ -1,5 +1,67 @@
 # frozen_string_literal: true
 
+# Load Terraform attributes
+project_id = attribute('project_id')
+service_account_email = attribute('service_account_email')
+available_memory_mb = attribute('available_memory_mb')
+timeout = attribute('timeout')
+
+event_configs = {
+  'http' => {
+    'function_name' => attribute('http_function_name_python'),
+    'runtime' => attribute('http_runtime_python'),
+    'vpc_connector' => attribute('vpc_connector')
+  },
+  'google.pubsub.topic.publish' => {
+    'function_name' => attribute('pubsub_function_name_python'),
+    'runtime' => attribute('pubsub_runtime_python'),
+    'vpc_connector' => attribute('vpc_connector')
+  },
+  'google.storage.object.finalize' => {
+    'function_name' => attribute('storage_function_name_python'),
+    'runtime' => attribute('storage_runtime_python'),
+    'vpc_connector' => attribute('vpc_connector')
+  }
+}
+
+control "cloud_functions" do
+  title "Check Cloud Functions"
+
+  event_configs.each do |event_type, event_config|
+    function_name = "projects/#{project_id}/locations/#{region}/functions/#{event_config['function_name']}"
+    vpc_connector = "projects/#{project_id}/locations/#{region}/connectors/#{event_config['vpc_connector']}"
+    describe google_cloudfunctions_cloud_function(project: project_id, location: region, name: event_config['function_name']) do
+      it { should exist }
+      its('name') { should eq function_name }
+      its('runtime') { should eq event_config['runtime'] }
+      its('status') { should eq "ACTIVE" }
+      its('service_account_email') { should eq service_account_email }
+      its('entry_point') { should eq entry_point }
+      its('available_memory_mb') { should eq available_memory_mb }
+      its('timeout') { should eq "#{timeout}s" } # Add 's' to match the format
+
+      # HTTP trigger
+      if event_type == 'http'
+        context "when using an HTTP trigger" do
+          it "should have an HTTPS trigger" do
+            expect(subject.https_trigger).not_to be_nil
+          end
+        end
+      # Pub/Sub and Storage triggers
+      else
+        context "when using a #{event_type.capitalize} trigger" do
+          it "should have an event trigger" do
+            expect(subject.event_trigger).not_to be_nil
+            expect(subject.event_trigger.event_type).to eq event_type
+          end
+        end
+      end
+    end
+  end
+end
+
+=begin # frozen_string_literal: true
+
 # Load YAML configuration
 require 'yaml'
 
@@ -54,3 +116,10 @@ control "cloud_functions" do
     end
   end
 end
+ =end
+
+
+
+
+
+ 
